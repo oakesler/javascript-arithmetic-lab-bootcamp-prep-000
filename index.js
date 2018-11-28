4d48df2ab2399b3b00fe94e145c6f3b5bb937801
Olivia Kesler <oakesler@protonmail.ch>
2018-11-28 01:51:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,17 +33,13 @@ function makeInt(n) {
     return "NaN"
 }
 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-
+function preserveDecimal(n) {
+  var x = parseFloat(n)
+  
+  if (x === n)
+    return x  
+    else return "NaN"
+}
 
 
 
