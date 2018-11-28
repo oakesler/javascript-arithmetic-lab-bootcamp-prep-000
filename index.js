e11269b9dcbdc86a7cea5f39ac6bc0b032dc1c59
Olivia Kesler <oakesler@protonmail.ch>
2018-11-28 00:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,17 @@ function makeInt(a) {
   var x = parseInt(a) 
   return (x /= 0)
 }
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
